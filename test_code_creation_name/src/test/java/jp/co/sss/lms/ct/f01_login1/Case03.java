0afe737e8102f6db03c6c29d3039782d4a76b71a
t-onuma <t-onuma@3sss.co.jp>
2025-10-24 14:42:44 +0900

    add: Case03.java 受講生ログイン正常系テスト追加
    ケース03：受講生ログイン正常系
    ConfigUtilsから取得した有効なログインID・パスワードを使用し、受講生ユーザーが正常にログインしコース詳細画面へ遷移できることを確認するテストを作成。
    
    期待値は以下の通り
    ・URLアクセス後、ログイン画面が表示されること
    ・正しいユーザー情報でログインするとコース詳細画面に遷移すること

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+
+import jp.co.sss.lms.ct.util.ConfigUtils;
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +39,40 @@ public class Case03 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		// ログイン画面を開く
+		goTo("http://localhost:8080/lms/");
+
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		String title = WebDriverUtils.webDriver.getTitle();
+
+		// 成功判定
+		assertTrue(currentUrl.contains("/lms/") && title.contains("ログイン"),
+				"ログイン画面への遷移に失敗しました");
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
-	}
+	void test02() throws Exception {
+		// ConfigUtilsからIDとパスワードを取得
+		String loginId = ConfigUtils.getSuccessLoginId();
+		String password = ConfigUtils.getSuccessPassword();
+		WebDriverUtils.webDriver.findElement(By.id("loginId")).sendKeys(loginId);
+		WebDriverUtils.webDriver.findElement(By.id("password")).sendKeys(password);
 
+		getEvidence("Case03/02-1.Login_Before");
+
+		// ログインボタンをクリック
+		WebDriverUtils.webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']")).click();
+		Thread.sleep(2000);
+		String currentUrl = WebDriverUtils.webDriver.getCurrentUrl();
+		String title = WebDriverUtils.webDriver.getTitle();
+
+		getEvidence("Case03/02-2.Login_Success");
+
+		// 成功判定
+		assertTrue(currentUrl.contains("/course/detail") && title.contains("コース詳細"),
+				"コース詳細画面への遷移に失敗しました");
+	}
 }
